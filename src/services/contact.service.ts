07f6baf9aa93839aee6ff9e441e966212c144a50
Daniel Forson <danielkojoforson27@gmail.com>
2025-12-15 15:38:34 +0000

    feat: implement contact service with CRUD operations

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -1,4 +1,26 @@
-import { DB_Connection } from "../config/data-source";
+import { Repository } from "typeorm";
 import { ContactEntity } from "../entities/contact.entity";
 
-const contactRepository = DB_Connection.getRepository(ContactEntity);
+export class ContactService { 
+    constructor(private contactRepository : Repository<ContactEntity>){}
+
+    async createContact(data: ContactEntity): Promise<void> { 
+        const newContact = this.contactRepository.create(data);
+        await this.contactRepository.save(newContact);
+    }
+    async getAllContacts(): Promise<ContactEntity[]> {
+        const allContactInfo = await this.contactRepository.find();
+    return allContactInfo;
+    }
+    
+    async deleteContactInfo(id: string): Promise<void| {message: string}> {
+        const contactToDelete = await this.contactRepository.findOne({ where: { id } });
+        if (!contactToDelete) { 
+           return {message: "Contact not found"};
+        } else {
+            await this.contactRepository.remove(contactToDelete);
+            return {message: "Contact deleted successfully"};
+        }
+    }
+
+}
