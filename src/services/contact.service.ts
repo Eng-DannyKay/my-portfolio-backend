ecf499189e6714025d70edab2f1b21ea956e8fda
Daniel Forson <danielkojoforson27@gmail.com>
2025-12-16 13:01:00 +0000

    feat: implement contact routes and update ContactService to return standardized response

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -1,19 +1,21 @@
 import { Repository } from "typeorm";
 import { ContactEntity } from "../entities/contact.entity";
+import { MessageRes } from "../dtos/contact.dto";
 
 export class ContactService { 
     constructor(private contactRepository : Repository<ContactEntity>){}
 
-    async createContact(data: ContactEntity): Promise<void> { 
+    async createContact(data: ContactEntity): Promise<MessageRes> { 
         const newContact = this.contactRepository.create(data);
         await this.contactRepository.save(newContact);
+        return {message: "Your Message sent successfully"};
     }
     async getAllContacts(): Promise<ContactEntity[]> {
         const allContactInfo = await this.contactRepository.find();
     return allContactInfo;
     }
     
-    async deleteContactInfo(id: string): Promise<void| {message: string}> {
+    async deleteContactInfo(id: string): Promise<void | MessageRes> {
         const contactToDelete = await this.contactRepository.findOne({ where: { id } });
         if (!contactToDelete) { 
            return {message: "Contact not found"};
