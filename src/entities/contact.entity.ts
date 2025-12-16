8da4245ce22900644301d4a3c2b0c200a98cd587
Daniel Forson <danielkojoforson27@gmail.com>
2025-12-16 13:00:38 +0000

    feat: enhance ContactEntity validation with decorators for name, subject, and message fields

--- src/entities/contact.entity.ts
+++ src/entities/contact.entity.ts
@@ -1,16 +1,27 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class ContactEntity{
     @PrimaryGeneratedColumn('uuid')
-    id: string;
+    id!: string;
 
     @Column()
-    name: string;
+    @IsNotEmpty({ message: "Name is required" })
+    @IsString({ message: "Name must be a string" })
+    name!: string;
+
+    @IsEmail({}, { message: "Invalid email address" })
+    @Column()
+    email!: string;
 
     @Column()
-    email: string;
+     @IsNotEmpty({ message: "Subject is required" })
+    @IsString({ message: "Subject must be a string" })
+    subject!: string;
 
     @Column()
-    message: string;
+    @IsNotEmpty({ message: "Message is required" })
+    @IsString({ message: "Message must be a string" })
+    message!: string;
 }
