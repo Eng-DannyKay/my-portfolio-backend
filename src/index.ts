7abd2911ae244a746d55fe9b6b67d5afa775a9ad
Daniel Forson <danielkojoforson27@gmail.com>
2025-12-17 08:21:03 +0000

    feat: update package.json and package-lock.json to include TypeScript and adjust build scripts; add initial server setup in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import express from "express";
 import cors from "cors";
-import { httpLogger } from "./src/middlewares/http-logger.middleware";
-import { DB_Connection } from "./src/config/data-source";
-import contactRoutes from "./src/routes/contact.routes";
+import { httpLogger } from "./middlewares/http-logger.middleware";
+import { DB_Connection } from "./config/data-source";
+import contactRoutes from "./routes/contact.routes";
 const app = express();
 const port = 3000;
 
