fa099e43ff4b510e4452464d8ed57112cd60c11e
Daniel Forson <danielkojoforson27@gmail.com>
2026-01-17 16:21:52 +0000

    fix: update CORS origin to use the correct URL for deployment

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://danielforson-portfolio.netlify.app"
+      "https://danielforson.onrender.com/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
