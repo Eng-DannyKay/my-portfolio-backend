cbdda6228ac5e9e35b708b4ac092beea0ebcb8af
Daniel Forson <danielkojoforson27@gmail.com>
2026-01-17 16:37:16 +0000

    fix: update CORS origin URL to remove trailing slash

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://danielforson.onrender.com/"
+      "https://danielforson.onrender.com"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
